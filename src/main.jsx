e55ed0f6f1c577c9514152a85983f00f7c045fe7
MuhammadAwais989 <officialawais989@gmail.com>
2025-12-10 02:15:23 -0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import React from 'react'
+import { Analytics } from "@vercel/analytics/next"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
